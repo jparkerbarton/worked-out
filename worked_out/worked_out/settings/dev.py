2bd89727f5ce0cc2e2ab99791625627a8cd5ddd1
Joel Barton <jparkerbarton@gmail.com>
2025-09-28 18:59:50 -0500

    Change database to PostgreSQL

--- worked_out/worked_out/settings/dev.py
+++ worked_out/worked_out/settings/dev.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,11 +84,15 @@ WSGI_APPLICATION = 'worked_out.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation

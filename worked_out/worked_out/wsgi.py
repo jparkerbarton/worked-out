f6eafbf15bd1283b676a785d63980d843aa0784b
Joel Barton <jparkerbarton@gmail.com>
2025-09-28 17:18:03 -0500

    Change wsgi.py

--- worked_out/worked_out/wsgi.py
+++ worked_out/worked_out/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'worked_out.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'worked_out.settings.prod')
 
 application = get_wsgi_application()

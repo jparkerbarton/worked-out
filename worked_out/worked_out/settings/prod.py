036f8fba071c5652bfddd145ec1ef2380b52e901
Joel Barton <jparkerbarton@gmail.com>
2025-09-28 16:22:52 -0500

    Install whitenoise and configure static files

--- worked_out/worked_out/settings/prod.py
+++ worked_out/worked_out/settings/prod.py
@@ -1,3 +1,20 @@
 from worked_out.settings.dev import *
 
-BASE_URL = []
+ALLOWED_HOSTS = ['worked-out-8b1f625e599b.herokuapp.com',]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
